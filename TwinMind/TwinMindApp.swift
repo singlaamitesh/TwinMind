6f653352baf7c944cb1d17c1d2b0866e697242d8
singlaamitesh <singlaamitesh@gmail.com>
2026-03-11 10:32:56 +0530

    Initial Commit

---
+++ TwinMind/TwinMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwinMindApp.swift
+//  TwinMind
+//
+//  Created by Amitesh Gupta on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TwinMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TwinMindTests/TwinMindTests.swift
@@ -0,0 +1,17 @@
+//
+//  TwinMindTests.swift
+//  TwinMindTests
+//
+//  Created by Amitesh Gupta on 11/03/26.
+//
+
+import Testing
+@testable import TwinMind
+
+struct TwinMindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
